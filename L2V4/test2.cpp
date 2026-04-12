21c4234ffe58c67c5e00ffb7e9275dea03766132
Nort-Fog <severfog5690jp@gmail.com>
2026-04-12 16:16:04 +0300

    L2V4

--- L2V4/test2.cpp
+++ L2V4/test2.cpp
@@ -3,13 +3,13 @@
  
 void main() 
 { 
-  TMatrix<int> a(5), b(5), c(5); 
+  TMatrix<int> a(10), b(10), c(10); 
   int i, j, g; 
  
   setlocale(LC_ALL, "Russian"); 
   cout << "Тестирование программ поддержки треугольных матриц" << endl; 
-  for (i = 0; i < 5; i++) 
-    for (j = i; j < 5; j++ )  
+  for (i = 0; i < 10; i++) 
+    for (j = i; j < 10; j++ )  
     { 
       a(i, j) =  i * 10 + j; 
       b(i, j) = (i * 10 + j) * 100; 
