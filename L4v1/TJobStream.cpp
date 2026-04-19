50ab1b4446b5ac2a3424fe1a51506bf252a895f6
Nort-Fog <severfog5690jp@gmail.com>
2026-04-19 17:25:58 +0300

    1725 l4

--- L4v1/TJobStream.cpp
+++ L4v1/TJobStream.cpp
@@ -1,5 +1,8 @@
 #include "TJobStream.h"
 #include <cstdlib>
+#include <iostream>
+using namespace std;
+
 
 TJobStream::TJobStream(int Intens) {
     JobIntåns = Intens / 100.0;  
@@ -8,9 +11,15 @@ TJobStream::TJobStream(int Intens) {
 int TJobStream::GetNewJob() {
 
     int r = rand() % 100;
+    
+    if (r < JobIntåns * 100.00) {
+        cout <<"q = " << r<< " ïîñòóïèëà \n"<< endl;
+        cout <<"Ïîðîã = " << JobIntåns * 100.00 << " \n"<< endl;
+        
 
-    if (r < JobIntåns * 100) {
         return 1;  
     } 
+    cout <<"q = " << r<< " ÍÅ ïîñòóïèëà \n"<< endl;
+    cout <<"Ïîðîã = " << JobIntåns * 100.00 << " \n"<< endl;
     return 0;  
 }
